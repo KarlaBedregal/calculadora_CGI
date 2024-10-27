c9daac11f300febf0c2545d36ce72b2203bde1ab
KarlaBedregal <kbedregalc@unsa.edu.pe>
2024-10-27 14:33:32 -0500

    Ultimos cambios
    Agregando últimos cambios a los archivos

--- cgi-bin/calculadora.pl
+++ cgi-bin/calculadora.pl
@@ -3,25 +3,71 @@ use strict;
 use warnings;
 use CGI qw(:standard);
 
+# Encabezado html que indica que el código es HTML con codificación UTF-8
 print header('text/html', 'UTF-8');
 
 # Obtener la operación del formulario
 my $operacion = param('operacion');
 my $resultado;
 
-# Procesar la operación
+# Procesar la operación si está presente
 if ($operacion) {
-    # Reemplazar 'raiz' por 'sqrt' para permitir la evaluación
-    $operacion =~ s/raiz\(([^)]+)\)/sqrt($1)/g;
+    # Resolver la expresión usando funciones sin 'eval'
+    $resultado = resolver_expresion($operacion);
+}
 
-    eval {
-        $resultado = eval $operacion;  # Evaluar la expresión matemática
-    };
-    
-    # Manejar errores en la evaluación
-    if ($@) {
-        $resultado = "Error en la operación: $@";
+# Definir funciones para operaciones matemáticas básicas
+sub sumar { my ($a, $b) = @_; return $a + $b; }
+sub restar { my ($a, $b) = @_; return $a - $b; }
+sub multiplicar { my ($a, $b) = @_; return $a * $b; }
+sub dividir { my ($a, $b) = @_; return $b != 0 ? $a / $b : "Error: División por cero"; }
+sub raiz_cuadrada { my ($a) = @_; return $a >= 0 ? sqrt($a) : "Error: Raíz de número negativo"; }
+
+# Función para resolver la expresión
+sub resolver_expresion {
+    my ($expr) = @_;
+
+    # Eliminar espacios
+    $expr =~ s/\s+//g;
+
+    # Evaluar expresiones de raíz cuadrada
+    while ($expr =~ /raiz\(([^()]+)\)/) {
+        my $sub_expr = $1;
+        my $sub_result = resolver_expresion($sub_expr);  # Evaluar dentro de la raíz
+        my $raiz_result = raiz_cuadrada($sub_result);
+        $expr =~ s/raiz\(\Q$sub_expr\E\)/$raiz_result/;
     }
+
+    # Evaluar primero los paréntesis internos
+    while ($expr =~ /\(([^()]+)\)/) {
+        my $sub_expr = $1;
+        my $sub_result = evaluar_operacion($sub_expr);
+        $expr =~ s/\(\Q$sub_expr\E\)/$sub_result/;
+    }
+
+    # Evaluar la operación final si no quedan paréntesis
+    return evaluar_operacion($expr);
+}
+
+# Función para evaluar operaciones simples sin paréntesis
+sub evaluar_operacion {
+    my ($expr) = @_;
+
+    # Resolver multiplicación y división primero
+    while ($expr =~ /(-?\d+(?:\.\d+)?)\s*([\*\/])\s*(-?\d+(?:\.\d+)?)/) {
+        my ($left, $op, $right) = ($1, $2, $3);
+        my $result = $op eq '*' ? multiplicar($left, $right) : dividir($left, $right);
+        $expr =~ s/\Q$left$op$right\E/$result/;
+    }
+
+    # Luego resolver suma y resta
+    while ($expr =~ /(-?\d+(?:\.\d+)?)\s*([\+\-])\s*(-?\d+(?:\.\d+)?)/) {
+        my ($left, $op, $right) = ($1, $2, $3);
+        my $result = $op eq '+' ? sumar($left, $right) : restar($left, $right);
+        $expr =~ s/\Q$left$op$right\E/$result/;
+    }
+
+    return $expr;
 }
 
 # Imprimir la salida HTML
@@ -55,13 +101,13 @@ print <<HTML;
 <footer class="footer">
     <div class="footer-links">
         <a href="https://validator.w3.org/#validate_by_input" class="image-link" target="_blank">
-            <img src="../img/W3C_HTML5_certified.png" alt="Markup Validation" />
+            <img src="../img/W3C_HTML5_certified.png" alt="Markup Validation" >
         </a>
         <a href="https://jigsaw.w3.org/css-validator/#validate_by_input" class="image-link" target="_blank">
-            <img src="../img/Valid_CSS_(blue).svg.png" alt="CSS Validation" />
+            <img src="../img/Valid_CSS_(blue).svg.png" alt="CSS Validation" >
         </a>
         <a href="https://www.w3.org/WAI/test-evaluate/tools/list/" class="image-link" target="_blank">
-            <img src="../img/wcag2.2AAA-blue.png" alt="Web Accessibility" />
+            <img src="../img/wcag2.2AAA-blue.png" alt="Web Accessibility" >
         </a>
     </div>
     <div>
